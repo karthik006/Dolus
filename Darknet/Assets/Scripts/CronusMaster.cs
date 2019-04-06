f62c87aea77c4a3746722526c48aaadab22217bf
Karthikeyan <karthikeyanp.98@gmail.com>
2019-04-06 22:14:52 +0530

    Added files

--- Darknet/Assets/Scripts/CronusMaster.cs
+++ Darknet/Assets/Scripts/CronusMaster.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
 
 public class CronusMaster : MonoBehaviour
 {
+    [SerializeField]
+    Text text;
+
+    [SerializeField]
+    GameObject backButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.enabled = false;
+        backButton.SetActive(false);  
     }
 
     // Update is called once per frame
@@ -16,8 +25,20 @@ public class CronusMaster : MonoBehaviour
         
     }
 
-    void OpenJournal()
+    public void OpenJournal1()
     {
-        
+        text.enabled = true;
+        text.text = "";
+        backButton.SetActive(true);
+        string path = "Assets/Data/Journal1.txt";
+        StreamReader streamReader = new StreamReader(path);
+
+        while (!streamReader.EndOfStream)
+        {
+            string line = streamReader.ReadLine();
+            text.text += line;
+        }
+
+        streamReader.Close();
     }
 }
